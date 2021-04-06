809318b144669f990ae797e130dfe58bfc725a2b
irafai <49647033+irafai@users.noreply.github.com>
2021-04-06 22:22:54 +0200

    Update choicemod.R

--- R/choicemod.R
+++ R/choicemod.R
@@ -87,8 +87,6 @@ choicemod <- function(X, Y, method = c("linreg","sir","rf"), N = 20,
       stop("\"nbsel\" must be a positive integer
         between 1 and p", call. = FALSE)
   
-  if(parallel & myCluster != NULL){
-    stop("you have to create a cluster prior to parallelizaion", call.=FALSE)}
   X <- as.matrix(X)
   n <- nrow(X)
   n_train <- round(prop_train*n, digits = 0)
