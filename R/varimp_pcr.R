da3a849bfa56be159c11fcec697d5e7f721acba6
irafai <49647033+irafai@users.noreply.github.com>
2021-04-06 19:33:59 +0200

    Update varimp_pcr.R

--- R/varimp_pcr.R
+++ R/varimp_pcr.R
@@ -1,7 +1,9 @@
 ######################
 # PCR
 ######################
-varimp_pcr <- function(X, Y, nrep=10){
+varimp_pcr <- function(X, Y, nrep=10,
+                       parallel=TRUE,numCores=parallel::detectCores()
+                      ){
   n <- nrow(X)
   model <- pls::pcr(Y~., data = data.frame(X),
     validation = "LOO", scale=FALSE)
@@ -18,7 +20,7 @@ varimp_pcr <- function(X, Y, nrep=10){
   n <- nrow(X)
   mat_mse <- matrix(0,nrow = nrep,ncol = p)
   colnames(mat_mse) <- colnames(X)
-
+  if (!parallel){
   for (r in 1:nrep) {
     for (j in 1:p){
       Xperm <- X
@@ -34,5 +36,23 @@ varimp_pcr <- function(X, Y, nrep=10){
       mat_mse[r,j] <- mean((Y - Ypred)^2)
     }
   }
+  }
+  if (parallel){
+    for (j in 1:p){
+      mat_mse[,j]<-foreach::foreach(r =c(1:nrep), .combine = 'c') %dopar% {
+	  Xperm <- X
+      Xperm[,j] <- Xperm[sample(1:n),j]
+
+      model <- pls::pcr(Y~., data = data.frame(Xperm),
+        validation = "CV", scale=FALSE)
+      # rmsep <- pls::RMSEP(model, intercept = FALSE)$val["CV",,]
+      rmsep <- sqrt(model$validation$PRESS/n)
+      ncomp <- find.cpt(rmsep)
+      Ypred <- as.vector(stats::predict(model, data.frame(Xperm),
+        ncomp=ncomp))
+        mean((Y - Ypred)^2)
+        }
+      }
+    }
   list(mat_mse = mat_mse,base_mse = base_mse)
 }
