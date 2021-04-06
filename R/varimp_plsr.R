4a1f845768902f271d904465eefb8ebdca75ae50
irafai <49647033+irafai@users.noreply.github.com>
2021-04-06 19:36:17 +0200

    Update varimp_plsr.R

--- R/varimp_plsr.R
+++ R/varimp_plsr.R
@@ -1,7 +1,10 @@
 ######################
 # PLSR
 ######################
-varimp_plsr <- function(X, Y, nrep=10){
+varimp_plsr <- function(X, Y, nrep=10,
+                        parallel=TRUE,numCores=parallel::detectCores()
+                      ){
+
   n <- nrow(X)
   model <- pls::plsr(Y~., data = data.frame(X),
     validation = "CV", scale=TRUE)
@@ -18,19 +21,36 @@ varimp_plsr <- function(X, Y, nrep=10){
   mat_mse <- matrix(0,nrow = nrep,ncol = p)
   colnames(mat_mse) <- colnames(X)
 
- for (r in 1:nrep) {
+   if (!parallel){
+     for (r in 1:nrep) {
+         for (j in 1:p){
+          Xperm <- X
+          Xperm[,j] <- Xperm[sample(1:n),j]
+          model <- pls::plsr(Y~., data = data.frame(Xperm),
+            validation = "CV", scale=TRUE)
+          # rmsep <- pls::RMSEP(model, intercept = FALSE)$val["CV",,]
+          rmsep <- sqrt(model$validation$PRESS/n)
+          ncomp <- find.cpt(rmsep)
+          Ypred <- as.vector(stats::predict(model, data.frame(Xperm),
+            ncomp=ncomp))
+          mat_mse[r,j] <- mean((Y - Ypred)^2)
+        }
+      }
+   }
+  else if (parallel){
     for (j in 1:p){
-      Xperm <- X
-      Xperm[,j] <- Xperm[sample(1:n),j]
-      model <- pls::plsr(Y~., data = data.frame(Xperm),
-        validation = "CV", scale=TRUE)
-      # rmsep <- pls::RMSEP(model, intercept = FALSE)$val["CV",,]
-      rmsep <- sqrt(model$validation$PRESS/n)
-      ncomp <- find.cpt(rmsep)
-      Ypred <- as.vector(stats::predict(model, data.frame(Xperm),
-        ncomp=ncomp))
-      mat_mse[r,j] <- mean((Y - Ypred)^2)
+      mat_mse[,j]<-foreach::foreach(r =c(1:nrep), .combine = 'c') %dopar% {
+	        Xperm <- X
+          Xperm[,j] <- Xperm[sample(1:n),j]
+          model <- pls::plsr(Y~., data = data.frame(Xperm),validation = "CV", scale=TRUE)
+          # rmsep <- pls::RMSEP(model, intercept = FALSE)$val["CV",,]
+          rmsep <- sqrt(model$validation$PRESS/n)
+          ncomp <- find.cpt(rmsep)
+          Ypred <- as.vector(stats::predict(model, data.frame(Xperm),
+            ncomp=ncomp))
+	         mean((Y - Ypred)^2)
+        }
+      }
     }
-  }
   list(mat_mse = mat_mse,base_mse = base_mse)
 }
